6b3d860436f81eb64970c4581d2b2ce867eda03a
Ivan Odak <ivanodak7776@hotmail.com>
2022-02-11 22:12:53 +0100

    small thing

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const cors = require("cors");
+const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const app = require("../src/routes/router");
+const recipeRouter = require("../src/routes/recipe");
 
 const env = dotenv.config();
+const app = express();
 
 const devUrl = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.zzejt.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 const testUrl = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.zzejt.mongodb.net/${process.env.MONGO_DB_TEST}?retryWrites=true&w=majority`;
@@ -41,11 +43,12 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(router);
+app.use("/api/recipes", recipeRouter);
 
 console.log(`Server running at http://${hostname}:${port}`);
 
 module.exports = {
+  app,
   db,
   dbConnection,
 };
